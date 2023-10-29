300165a2dc69b343647c89430fd99bdfb610de91
Barsha76 <sumiyabarsha992@gmail.com>
2023-10-29 18:38:39 +0600

    update2

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 
 class Register extends StatefulWidget {
@@ -10,6 +12,18 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref().child("Users");
+
+  // Controllers for input fields
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,6 +71,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: emailController,
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
             hintText: 'Enter your email',
@@ -79,6 +94,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: nameController,
           decoration: InputDecoration(
             hintText: 'Enter your name',
           ),
@@ -100,6 +116,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: passwordController,
           obscureText: true,
           decoration: InputDecoration(
             hintText: 'Enter your password',
@@ -122,6 +139,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: confirmPasswordController,
           obscureText: true,
           decoration: InputDecoration(
             hintText: 'Confirm your password',
@@ -144,6 +162,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: heightController,
           keyboardType: TextInputType.number,
           decoration: InputDecoration(
             hintText: 'Enter your height in cm',
@@ -166,6 +185,7 @@ class _RegisterState extends State<Register> {
           ),
         ),
         TextFormField(
+          controller: weightController,
           keyboardType: TextInputType.number,
           decoration: InputDecoration(
             hintText: 'Enter your weight in kg',
@@ -180,8 +200,51 @@ class _RegisterState extends State<Register> {
       padding: EdgeInsets.symmetric(vertical: 25),
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {
-          // Add your registration logic here
+        onPressed: () async {
+          if (passwordController.text == confirmPasswordController.text) {
+            try {
+              UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+                email: emailController.text,
+                password: passwordController.text,
+              );
+
+              // If registration is successful, save additional user data to Firebase Realtime Database
+              if (userCredential.user != null) {
+                _dbRef.child(userCredential.user!.uid).set({
+                  'name': nameController.text,
+                  'email': emailController.text,
+                  'height': heightController.text,
+                  'weight': weightController.text,
+                });
+                await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Registration'),
+                      content: Text('Registration successful!'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+
+                Navigator.pop(context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              }
+            } catch (e) {
+              print(e); // Print the exception for debugging
+              // Display an error message to the user
+            }
+          } else {
+            // Display an error message if passwords don't match
+          }
         },
         style: ElevatedButton.styleFrom(
           backgroundColor: Color(0xff3ba80b), // Button color
